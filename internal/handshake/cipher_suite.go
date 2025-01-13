bec44eaddff7beb2044fc569f421d3dc2f2f2297
lneto <lneto@cloudflare.com>
2025-01-13 15:34:31 +0000

    Support BoringSSL in QUIC-GO when encrypting packets
    Add support for BoringSSL which is controlled by an environment variable.
    The changes also include the logic that when the first nonce of xorNonceAEAD isn't the nonce = 0 then it provides one.

--- internal/handshake/cipher_suite.go
+++ internal/handshake/cipher_suite.go
@@ -3,16 +3,27 @@ package handshake
 import (
 	"crypto"
 	"crypto/aes"
+	"crypto/boring"
 	"crypto/cipher"
 	"crypto/tls"
+	"errors"
 	"fmt"
+	"os"
 
 	"golang.org/x/crypto/chacha20poly1305"
 )
 
 // These cipher suite implementations are copied from the standard library crypto/tls package.
 
-const aeadNonceLength = 12
+const (
+	aeadNonceLength = 12
+)
+
+var (
+	useBoring             = len(os.Getenv("USE_BORING")) != 0
+	errBoringIsNotEnabled = errors.New("boring was requested but not enabled")
+	zeroNonce             = [aeadNonceLength]byte{}
+)
 
 type cipherSuite struct {
 	ID     uint16
@@ -44,12 +55,21 @@ func aeadAESGCMTLS13(key, nonceMask []byte) *xorNonceAEAD {
 	if err != nil {
 		panic(err)
 	}
-	aead, err := cipher.NewGCM(aes)
+	var aead cipher.AEAD
+	if useBoring {
+		if boring.Enabled() {
+			aead, err = tls.NewGCMTLS13(aes)
+		} else {
+			err = errBoringIsNotEnabled
+		}
+	} else {
+		aead, err = cipher.NewGCM(aes)
+	}
 	if err != nil {
 		panic(err)
 	}
 
-	ret := &xorNonceAEAD{aead: aead}
+	ret := &xorNonceAEAD{aead: aead, hasSeenNonceZero: false}
 	copy(ret.nonceMask[:], nonceMask)
 	return ret
 }
@@ -71,15 +91,52 @@ func aeadChaCha20Poly1305(key, nonceMask []byte) *xorNonceAEAD {
 // xorNonceAEAD wraps an AEAD by XORing in a fixed pattern to the nonce
 // before each call.
 type xorNonceAEAD struct {
-	nonceMask [aeadNonceLength]byte
-	aead      cipher.AEAD
+	nonceMask        [aeadNonceLength]byte
+	aead             cipher.AEAD
+	hasSeenNonceZero bool // This value denotes if the aead field was used with a nonce = 0
 }
 
 func (f *xorNonceAEAD) NonceSize() int        { return 8 } // 64-bit sequence number
 func (f *xorNonceAEAD) Overhead() int         { return f.aead.Overhead() }
 func (f *xorNonceAEAD) explicitNonceLen() int { return 0 }
 
+func allZeros(nonce []byte) bool {
+	for _, e := range nonce {
+		if e != 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func (f *xorNonceAEAD) Seal(out, nonce, plaintext, additionalData []byte) []byte {
+	if useBoring {
+		if boring.Enabled() {
+			if !f.hasSeenNonceZero {
+				// BoringSSL expects that the first nonce passed to the
+				// AEAD instance is zero.
+				// At this point the nonce argument is either zero or
+				// an artificial one will be passed to the AEAD through
+				// [sealZeroNonce]
+				f.hasSeenNonceZero = true
+				if !allZeros(nonce) {
+					f.sealZeroNonce()
+				}
+			}
+		} else {
+			panic(errBoringIsNotEnabled)
+		}
+	}
+
+	return f.seal(nonce, out, plaintext, additionalData)
+}
+
+func (f *xorNonceAEAD) sealZeroNonce() {
+	zeroNonce := [aeadNonceLength]byte{}
+	f.seal([]byte{}, zeroNonce[:], []byte{}, []byte{})
+}
+
+func (f *xorNonceAEAD) seal(nonce []byte, out []byte, plaintext []byte, additionalData []byte) []byte {
 	for i, b := range nonce {
 		f.nonceMask[4+i] ^= b
 	}
